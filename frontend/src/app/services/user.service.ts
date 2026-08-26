5f1e5bdbfa6311a2e43c6ad13ea965a2748f841a
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2026-08-25 22:29:52 -0500

    configura perfiles desarrollo y prod

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -2,13 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private readonly http = inject(HttpClient);
-  private readonly endpoint = '/api/clients';
+  private readonly endpoint = `${environment.apiBaseUrl}/clients`;
 
   findAll(): Observable<User[]> {
     return this.http.get<User[]>(this.endpoint);
